877849733732050de0201c4dcec527e8bae9e21b
Andrew <630841816@qq.com>
2016-03-09 10:48:17 +0800

    update

---
+++ src/main/java/edu/xiyou/shotrent/dao/UserMapper.java
@@ -0,0 +1,17 @@
+package edu.xiyou.shotrent.dao;
+
+import edu.xiyou.shotrent.model.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
